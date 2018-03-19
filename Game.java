d7204e147602bff9e649c50fa8b7579abefac406
Huynh Nguyen Ngoc Linh <linh0ltv@gmail.com>
2018-03-19 17:09:30 +0700

    dk

--- Game.java
+++ Game.java
@@ -7,7 +7,7 @@ public class Game {
         try {
             final String os= System.getProperty("os.name");
             if(os.contains("Windows")) {
-                new ProcessBuilder("cls").inheritIO().start().waitFor();
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             }
             else {
                 new ProcessBuilder("clear").inheritIO().start().waitFor();
